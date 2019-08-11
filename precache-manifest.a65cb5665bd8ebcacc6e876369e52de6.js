01e75243ee37b2df1751da01d7aab5b785429a75
lalitmee <lalitkumar.meena.lk@gmail.com>
2019-08-12 00:27:33 +0530

    Updates

---
+++ precache-manifest.a65cb5665bd8ebcacc6e876369e52de6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96aa6dc8c31a3932978b38f312b5fee7",
+    "url": "/lalitmee.github.io/index.html"
+  },
+  {
+    "revision": "dbedc27d3e702e28db7f",
+    "url": "/lalitmee.github.io/static/css/2.f2d803be.chunk.css"
+  },
+  {
+    "revision": "29bd6cd41e77971a2648",
+    "url": "/lalitmee.github.io/static/css/main.1159541b.chunk.css"
+  },
+  {
+    "revision": "dbedc27d3e702e28db7f",
+    "url": "/lalitmee.github.io/static/js/2.3de6b45c.chunk.js"
+  },
+  {
+    "revision": "29bd6cd41e77971a2648",
+    "url": "/lalitmee.github.io/static/js/main.22296355.chunk.js"
+  },
+  {
+    "revision": "d38d793d7fa2d298e4bf",
+    "url": "/lalitmee.github.io/static/js/runtime~main.ff751385.js"
+  },
+  {
+    "revision": "c5241d5456b68a4418c6b17ce287d9cf",
+    "url": "/lalitmee.github.io/static/media/back5.c5241d54.png"
+  },
+  {
+    "revision": "0ab5a87e8ae0b324879ac2b89c2c4bcd",
+    "url": "/lalitmee.github.io/static/media/image-loader.0ab5a87e.gif"
+  },
+  {
+    "revision": "f1448054f14757d284c064b271c00cc6",
+    "url": "/lalitmee.github.io/static/media/proxima.f1448054.ttf"
+  },
+  {
+    "revision": "c0e7f075f9c77cfff95e401b449ec29a",
+    "url": "/lalitmee.github.io/static/media/resume-image.c0e7f075.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/lalitmee.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/lalitmee.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/lalitmee.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/lalitmee.github.io/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4cf4504c36425e2412049c4fa11ab45.js"
+  "/lalitmee.github.io/precache-manifest.a65cb5665bd8ebcacc6e876369e52de6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lalitmee.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
